075c5211003d5d36ce4af1f5db2e3c5b6be8ea8f
KaAter <katerelsherif@gmail.com>
2021-03-29 17:43:23 +0200

    updating compact card

--- src/components/GridCard.tsx
+++ src/components/GridCard.tsx
@@ -5,65 +5,94 @@ import details from "../assets/info_white_24dp.svg";
 interface Props {}
 
 const GridCard = (props: Props) => {
-  const [transitionClass, setTransitionClass] = useState("");
+  const [mainTransitionClass, setMainTransitionClass] = useState("");
+  const [secondaryTransitionClass, setSecondaryTransitionClass] = useState("");
   const [visibleClass, setVisibleClass] = useState("invisible");
 
   const forward = () => {
-    setTransitionClass("transform -translate-x-full transition-transform ");
+    setMainTransitionClass(" transform -translate-x-1/2 transition-transform ");
+    setSecondaryTransitionClass(
+      " transform translate-x-1/2 transition-transform "
+    );
     setVisibleClass("visible");
-    console.log(transitionClass);
+    console.log(mainTransitionClass);
   };
   const backward = () => {};
   return (
-    <div className="grid grid-cols-2 w-1/2 h-3/4 mx-auto bg-pink-300">
+    <div className="grid grid-cols-2 w-1/2 h-3/4 mx-auto  ml-32">
       <div
-        className={`col-start-2 col-end-3 row-start-1 row-end-2 grid grid-rows-12 text-white  bg-green-500 p-2  ${transitionClass}`}
+        className={`col-start-2 col-end-3 row-start-1 row-end-2 grid grid-rows-12 text-white  
+          bg-green-500 p-2 h-full  ${mainTransitionClass}`}
       >
-        <div className=" row-span-1 flex justify-between ">
-          <div>Cairo,Egypt</div>
-          <div
-            className="cursor-pointer active:bg-yellow-600  "
-            onClick={forward}
-          >
+        <div className=" row-span-1 flex justify-end  ">
+          <div className="cursor-pointer   " onClick={forward}>
             <img src={details}></img>
           </div>
         </div>
-        <div className=" row-span-8 my-auto">
-          <div className="text-center flex flex-col  space-y-2 ">
-            <div className="w-1/4 h-1/4 self-center ">
-              <img src={moon}></img>
-            </div>
-
-            <div className=" font-bold text-5xl">
-              23<sup className="text-base align-top">o</sup>
-            </div>
-            <div className="text-xl">Lorem, ipsum dolor.</div>
-          </div>
+        <div className=" text-center row-span-2 flex flex-col my-auto ">
+          {/* <div className="flex flex-col"> */}
+          <div className="text-4xl">Cairo,Egypt</div>
         </div>
-        <div className="row-span-3 self-center text-left flex-border w-5/6 mx-auto">
-          <div className="flex border-b-2 justify-between">
-            <div className=" border-white">Reel Feel Temprature</div>
+        <div className="text-center  row-span-6 flex flex-col my-auto">
+          <div className="w-1/4 h-1/4 self-center  ">
+            <img src={moon}></img>
+          </div>
+
+          <div className=" font-bold text-5xl">
+            23<sup className="text-base align-top">o</sup>
+          </div>
+          <div className="text-l">Lorem, ipsum dolor.</div>
+          {/* </div> */}
+        </div>
+        <div className="row-span-3 text-left w-5/6 mx-auto ">
+          <div className="flex border-b-2  justify-between ">
+            <div className=" border-white ">Reel Feel </div>
             <div>Degree</div>
           </div>
 
           <div className="flex border-b-2 justify-between">
-            <div className=" border-white">Shade Temprature</div>
+            <div className=" border-white">Shade </div>
             <div>Degree</div>
           </div>
           <div className="flex border-b-2 justify-between">
-            <div className=" border-white">Other Temprature</div>
+            <div className=" border-white">Other </div>
             <div>Degree</div>
           </div>
         </div>
       </div>
       <div
-        className={`bg-purple-500 h-full col-start-2 col-end-3 row-start-1 row-end-2 ${visibleClass} grid grid-cols-2 grid-rows-5 p-2`}
+        className={`bg-purple-400  col-start-2 col-end-3 row-start-1 row-end-2 ${visibleClass} grid grid-rows-8 p-2 items-center 
+        text-white text-sm  ${secondaryTransitionClass} `}
       >
-        <div className="col-span-2">1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
+        <div className=" text-center">Full Details</div>
+        <div className="flex border-b border-white border-dashed p-1 justify-between">
+          <div className=" ">Humidity</div>
+          <div>Value1</div>
+        </div>
+        <div className="flex border-b border-white  border-dashed p-1 justify-between">
+          <div className=" ">Relative Humidity</div>
+          <div>Value2</div>
+        </div>
+        <div className="flex border-b border-white  border-dashed p-1 justify-between">
+          <div className=" ">Wind Speed</div>
+          <div>Value3</div>
+        </div>
+        <div className="flex border-b border-white  border-dashed p-1 justify-between">
+          <div className=" ">UVIndex</div>
+          <div>Value4</div>
+        </div>
+        <div className="flex border-b border-white  border-dashed p-1 justify-between">
+          <div className=" ">Pressure</div>
+          <div>Value5</div>
+        </div>
+        <div className="flex border-b border-white  border-dashed p-1 justify-between">
+          <div className=" ">Pressure</div>
+          <div>Value5</div>
+        </div>{" "}
+        <div className="flex border-b border-white border-dashed p-1 justify-between">
+          <div className=" ">Pressure</div>
+          <div>Value5</div>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,27 @@ module.exports = {
       gridTemplateRows: {
         // Simple 8 row grid
         12: "repeat(12, minmax(0, 1fr))",
+        10: "repeat(12, minmax(0, 1fr))",
       },
       gridRow: {
         "span-8": "span 8 / span 8",
         "span-7": "span 7 / span 7",
+      },
+      gridRowStart: {
+        8: "8",
+        9: "9",
+        10: "10",
+        11: "11",
+        12: "12",
+        13: "13",
+      },
+      gridRowEnd: {
+        8: "8",
+        9: "9",
+        10: "10",
+        11: "11",
+        12: "12",
+        13: "13",
       },
     },
   },
